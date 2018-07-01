087da8db1442282ef939f29299972fa532ae9f12
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2018-07-01 15:00:48 +0200

    Add command line parameters to the LP Solver
    The LP Solver has been updated to use Boost's command line parameters to
    make it easier to use the program.

--- src/lp-solver.cc
+++ src/lp-solver.cc
@@ -1,56 +1,75 @@
 #include <iostream>
 #include <tinyxml2.h>
+#include <boost/program_options.hpp>
 
 #include "flow-manager.h"
 #include "graph-manager.h"
 #include "xml-utilities.h"
 
-int main (int argc, char *argv[])
+namespace po = boost::program_options;
+
+int main (int argc, const char *argv[])
 {
-  if (argc < 3 || argc > 4)
-    {
-      std::cerr << "Incorrect number of command-line arguments were passed." << std::endl;
-      return EXIT_FAILURE;
-    }
+    std::string solverConfiguration {""};
+    std::string lgfPath {""};
+    std::string xmlLogPath {""};
 
   try
+  {
+    po::options_description cmdLineParams("Allowed Options");
+    cmdLineParams.add_options()
+      ("help", "Help Message")
+      ("lgfPath", po::value<std::string>(&lgfPath)->required(),
+       "The path to the LGF file")
+      ("xmlLogPath", po::value<std::string>(&xmlLogPath)->required(),
+       "The path where to store the XML log file with the optimal solution")
+      ("solverConfig",
+       po::value<std::string>(&solverConfiguration)->default_value("mfmc"),
+       "The solver configuration\n"
+       "mfmc: MaxFlowMinCost\nmf: Max Flow\nmc: Minimum Cost");
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, cmdLineParams), vm);
+
+    if (vm.count("help")) // Output help message
     {
-      /**
-        * mfmc = Max Flow Minimum Cost
-        * mf = Max Flow
-        * mc = Minimum Cost
-        */
-      std::string solverConfiguration ("mfmc");
-      const std::string lgfPath (argv[1]);
-      const std::string xmlLogPath (argv[2]);
-      if (argc == 4) solverConfiguration = argv[3];
-
-      FlowManager flowManager;
-      flowManager.LoadFlowsFromFile (lgfPath);
-
-      GraphManager graphManager (flowManager.GetFlows());
-      graphManager.ParseGraph (lgfPath);
-      graphManager.VerifyFlows ();
-      graphManager.FindOptimalSolution (solverConfiguration);
-      graphManager.GenerateAckRoutes ();
-
-      // XML Logging
-      tinyxml2::XMLDocument xmlLogFile;
-      XmlUtilities::InsertRootNode (xmlLogFile, "Log");
-      XmlUtilities::InsertTimeStampInRootElement (xmlLogFile);
-      // Generating the Optimal Solution + Network Topology elements in the XML
-      // log file
-      graphManager.AddLogsInXmlFile (xmlLogFile);
-
-      // Save the XML file.
-      XmlUtilities::SaveXmlFile (xmlLogPath, xmlLogFile);
-
-      // Throw an error if the optimal solution is not found.
-      if (!graphManager.OptimalSolutionFound()) return EXIT_FAILURE;
+      std::cout << cmdLineParams << "\n";
+      return EXIT_SUCCESS;
     }
+    po::notify(vm); // Check if all required parameters are passed
+
+    FlowManager flowManager;
+    flowManager.LoadFlowsFromFile (lgfPath);
+
+    GraphManager graphManager (flowManager.GetFlows());
+    graphManager.ParseGraph (lgfPath);
+    graphManager.VerifyFlows ();
+    graphManager.FindOptimalSolution (solverConfiguration);
+    graphManager.GenerateAckRoutes ();
+
+    // XML Logging
+    tinyxml2::XMLDocument xmlLogFile;
+    XmlUtilities::InsertRootNode (xmlLogFile, "Log");
+    XmlUtilities::InsertTimeStampInRootElement (xmlLogFile);
+    // Generating the Optimal Solution + Network Topology elements in the XML
+    // log file
+    graphManager.AddLogsInXmlFile (xmlLogFile);
+
+    // Save the XML file.
+    XmlUtilities::SaveXmlFile (xmlLogPath, xmlLogFile);
+
+    // Throw an error if the optimal solution is not found.
+    if (!graphManager.OptimalSolutionFound()) return EXIT_FAILURE;
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Error: " << e.what() << "\n";
+    return EXIT_FAILURE;
+  }
   catch (...)
-    {
-      return EXIT_FAILURE;
-    }
+  {
+    return EXIT_FAILURE;
+  }
+
   return EXIT_SUCCESS;
 }
