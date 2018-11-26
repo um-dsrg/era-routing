b209a24c2388ed258ed51426bc8a0e94e4b3d9e9
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2018-11-26 21:58:24 +0100

    WIP Add path constrained LP expressions
    Add some basic LP variables and set some constraints for the path constrained LP based solver. Need to add the link capacity constraint.

--- PathConstrainedMCFPSolver/src/main.cc
+++ PathConstrainedMCFPSolver/src/main.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <tinyxml2.h>
+#include <lemon/lp.h>
 
 #include "links.h"
 #include "flows.h"
@@ -26,6 +27,64 @@ int main (int argc, const char *argv[])
   // Build the links
   Links links{rootNode};
   std::map<id_t, Flow> flows {PopulateFlowsFromXml(rootNode)};
+
+  // FIXME Move to function/class + optimise!
+  lemon::GlpkLp lpSolver;
+
+  // Assign an LP variable for each path
+  for (auto& flowTuple : flows)
+    {
+      for (auto& pathTuple: flowTuple.second.GetPaths())
+        {
+          id_t pathId = pathTuple.first;
+          std::cout << pathId << std::endl;
+
+          lemon::Lp::Col dataRateOnPath = lpSolver.addCol();
+          pathTuple.second.SetAssignedDataRateVariable(dataRateOnPath);
+
+          // Constraint that the data rate assigned on each path is positive
+          lpSolver.addRow(dataRateOnPath >= 0);
+        }
+    }
+
+  // Set the flow data rate constraint such that no flow is over provisioned
+  for (auto& flowTuple : flows)
+    {
+      Flow& flow {flowTuple.second};
+      lemon::Lp::Expr flowDataRateExpression;
+
+      for (auto& pathTuple: flow.GetPaths())
+        flowDataRateExpression += pathTuple.second.GetAssignedDataRateVariable();
+
+      lpSolver.addRow(flowDataRateExpression <= flow.GetDataRate());
+    }
+
+  // TODO: Set the link capacity constraint
+
+  // Set the solver to find the solution with the maximum value
+  lpSolver.max();
+
+  // Set the objective
+  lemon::Lp::Expr objective;
+
+  // Loop through all the flows, all the paths and sum all the variables together.
+  for (auto& flowTuple : flows) // Loop through all the flows
+    {
+      for (auto& pathTuple: flowTuple.second.GetPaths())
+        {
+          objective += pathTuple.second.GetAssignedDataRateVariable();
+        }
+    }
+
+  lpSolver.obj(objective);
+
+  // Solve the problem
+  lpSolver.solve();
+
+  if (lpSolver.primalType() == lemon::Lp::OPTIMAL)
+    std::cout << "Solution found" << std::endl;
+  else
+    std::cout << "Solution *NOT* found" << std::endl;
 
   return EXIT_SUCCESS;
 }
