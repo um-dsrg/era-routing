0effc6d71d6566ca74d9a44eb1571b49fdad0b8e
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2018-11-26 21:54:25 +0100

    Add LP Variable per path

--- PathConstrainedMCFPSolver/src/flows.h
+++ PathConstrainedMCFPSolver/src/flows.h
@@ -11,6 +11,7 @@
 #include <vector>
 #include <map>
 #include <tinyxml2.h>
+#include <lemon/lp.h>
 
 #include "definitions.h"
 
@@ -18,14 +19,21 @@ class Path {
 public:
   Path(tinyxml2::XMLElement* pathElement);
 
+  lemon::Lp::Col GetAssignedDataRateVariable() { return m_assignedDataRate; }
+  void SetAssignedDataRateVariable(lemon::Lp::Col dataRateVariable) { m_assignedDataRate = dataRateVariable; }
+
 private:
   double m_cost;
   std::vector<id_t> m_links;
+  lemon::Lp::Col m_assignedDataRate;
 };
 
 class Flow {
 public:
   Flow(tinyxml2::XMLElement* flowElement);
+
+  double GetDataRate() { return m_dataRate; }
+  std::map<id_t, Path>& GetPaths() { return m_paths; }
 
 private:
   void PopulateFlowFromXmlElement(tinyxml2::XMLElement* flowElement);
