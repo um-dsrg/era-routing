0cb5b1a23ef23a6bfb1a65f2f89185fda75caf43
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2018-12-20 17:47:13 +0100

    Update simulation to conform with new setup
    The simulation setup has been updated to use command line parameters,
    create a job script for the path constrained LP script and use the new
    format for calling the job script and flow set generator.

--- SimulationSetup/setup_obj_test.py
+++ SimulationSetup/setup_obj_test.py
@@ -4,34 +4,23 @@ Creates the necessary directory structure and files to run the testing of the
 new objectives setup.
 """
 import os
+import argparse
 import subprocess
 from pathlib import Path
 
 from modules.job_script import JobScript
 
 
-def create_flow_set(out_lgf_path, num_flow, net_load):
-    """Create a graph file with the specified details."""
-    home_dir = str(Path.home())
-    dev_dir = home_dir + '/Development/Development/SimulationSetup'
-    command = ['python3',
-               dev_dir + '/generate_random_flow_set.py',
-               '--base_lgf_file', dev_dir + '/base_geant.lgf',
-               '--save_lgf_file', out_lgf_path,
+def create_flow_set(flowset_generator_path: str, input_lgf_path: str, output_lgf_path: str, num_flow,
+                    network_load: str):
+    """Generate an LGF file with the specified details"""
+    command = ['python', flowset_generator_path,
+               '--input_lgf_path', input_lgf_path,
+               '--output_lgf_path', output_lgf_path,
                '--num_flows', str(num_flow),
                '--scaling_factor', '30',
                '--flow_protocol', 'T',
-               '--vid_res_prob']
-
-    # Vid flow resolution order: SD, HD, UHD
-    if net_load == 'high':
-        command.extend(['0.0', '0.0', '1.0'])
-    elif net_load == 'medium':
-        command.extend(['0.0', '1.0', '0.0'])
-    elif net_load == 'low':
-        command.extend(['1.0', '0.0', '0.0'])
-    else:
-        raise RuntimeError('The following network load is unknown {}'.format(net_load))
+               '--network_load', network_load]
 
     print(repr(command))
 
@@ -39,12 +28,11 @@ def create_flow_set(out_lgf_path, num_flow, net_load):
     if process_status.returncode != 0:
         raise RuntimeError('The following command returned with errors\n{}'.format(command))
 
-
 def create_lp_jobscript(net_load, num_flows, base_path, shell_graph_path, shell_base_path):
     """Creates the LP jobscript from the given parameters"""
     job_queue = 'short'
     job_name = 'lp_{}l_f_{}'.format(net_load[0], num_flows)
-    job_logs_dir = '${HOME}/logs/'  # Nyquist home directory
+    job_logs_dir = '${HOME}/logs/'  # Nyquist logs directory
     job_not_flags = 'a'
     job_email = 'noel.farrugia.09@um.edu.mt'
     lp_job = JobScript(job_queue, job_name, job_logs_dir, job_not_flags, job_email)
@@ -61,12 +49,11 @@ def create_lp_jobscript(net_load, num_flows, base_path, shell_graph_path, shell_
     print('Saving Lp job in {}'.format(lp_job_path))
     lp_job.save_job_script(lp_job_path)
 
-
 def create_ksp_jobscript(k_value, net_load, num_flows, base_path, shell_graph_path, shell_base_path):
     """Creates the KSP command and jobscript from the given parameters.
 
-    :return: The KSP result file path in shell format. This will be used when creating the genetic
-             algorithm jobscript.
+    :return: The KSP result file path in shell format. This will be used when creating jobscripts dependent on the
+             KSP file.
     """
     job_queue = 'short'
     job_name = 'ksp_{}l_f_{}_k_{}'.format(net_load[0], num_flows, k_value)
@@ -91,6 +78,26 @@ def create_ksp_jobscript(k_value, net_load, num_flows, base_path, shell_graph_pa
 
     return ksp_result_file
 
+def create_pc_lp_jobscript(net_load, num_flows, ksp_result_file, base_path, shell_base_path):
+    """Creates the Path Constrained LP jobscript from the given parameters"""
+    job_queue = 'short'
+    job_name = 'pc_lp_{}l_f_{}'.format(net_load[0], num_flows)
+    job_logs_dir = '${HOME}/logs/'  # Nyquist logs directory
+    job_not_flags = 'a'
+    job_email = 'noel.farrugia.09@um.edu.mt'
+    pc_lp_job = JobScript(job_queue, job_name, job_logs_dir, job_not_flags, job_email)
+
+    pc_lp_result_file = shell_base_path + '/result_pc_lp.xml'
+    command = ('${{HOME}}/Development/Development/PathConstrainedMCFPSolver/build/release/pc_mcfp_solver \\\n'
+               '    -i {} \\\n'
+               '    -o {}'.format(ksp_result_file, pc_lp_result_file))
+
+    print(repr(command))
+    pc_lp_job.insert_command(command)
+
+    pc_lp_job_path = base_path + '/run_pc_lp.pbs'
+    print('Saving Lp job in {}'.format(pc_lp_job_path))
+    pc_lp_job.save_job_script(pc_lp_job_path)
 
 def create_ga_jobscript(k_value, net_load, num_flows, ksp_result_file, base_path, shell_base_path):
     """Creates the GA command and jobscript from the given parameters."""
@@ -130,42 +137,52 @@ def main():
     # the shell. These are used when writing paths to the jobscript command where the
     # path will be interpreted by the shell not by the python interpreter.
 
-    home_dir = str(Path.home())
-    sp_objs_dir = 'GDrive/Scratchpad/genetic_algorithm/new_objectives'
-    base_directory = str(Path.home()) + '/' + sp_objs_dir
-    network_load = ['high', 'medium', 'low']
-    k_values = ['5']
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--base_dir', type=str, required=True,
+                        help='The path to the base scratchpad directory')
+    parser.add_argument('--shell_sp_dir', type=str, required=True,
+                        help='The directory to the sp folder to be used by the Linux shell')
+    parser.add_argument('--flowset_generator_path', type=str, required=True,
+                        help='The path to the flow script generator')
+    parser.add_argument('--input_lgf_path', type=str, required=True,
+                        help='The path to the base lgf file to be used')
+    parser.add_argument('--network_load', type=str, required=True, nargs='*', default=[],
+                        help='The network loads to generate')
+    parser.add_argument('--k_values', type=str, required=True, nargs='*', default=[],
+                        help='The K values to use for each flow set')
+    args = parser.parse_args()
 
     # Create the directories
-    for net_load in network_load:
+    for net_load in args.network_load:
         if net_load == 'high':  # The max num of flows under high load is 150
             num_flows = list(range(50, 151, 50))
         else:
             num_flows = list(range(50, 301, 50))
 
         for num_flow in num_flows:
-            shell_base_dir = '${HOME}/' + sp_objs_dir  # Used when writing the path to be used by a shell
-            curr_dir = base_directory + '/load_' + net_load + '/flows_' + str(num_flow)
-            shell_curr_dir = shell_base_dir + '/load_' + net_load + '/flows_' + str(num_flow)
+            current_dir = args.base_dir + '/load_' + net_load + '/flows_' + str(num_flow)
+            shell_current_dir = args.shell_sp_dir + '/load_' + net_load + '/flows_' + str(num_flow)
 
-            print('Creating directory {}'.format(curr_dir))
-            os.makedirs(curr_dir)
+            print('Creating directory {}'.format(current_dir))
+            os.makedirs(current_dir, exist_ok=True)
 
-            graph_path = curr_dir + '/graph.lgf'
-            shell_graph_path = shell_curr_dir + '/graph.lgf'
+            output_lgf_path = current_dir + '/graph.lgf'
+            shell_graph_path = shell_current_dir + '/graph.lgf'
 
             # Create the graphs with the desired flow distribution
-            create_flow_set(graph_path, num_flow, net_load)
+            create_flow_set(args.flowset_generator_path, args.input_lgf_path, output_lgf_path, num_flow, net_load)
 
             # Create Lp Solver job script
-            create_lp_jobscript(net_load, num_flow, curr_dir, shell_graph_path, shell_curr_dir)
+            create_lp_jobscript(net_load, num_flow, current_dir, shell_graph_path, shell_current_dir)
 
-            # Create the KSP jobscripts
-            for k_value in k_values:
-                ksp_res_file = create_ksp_jobscript(k_value, net_load, num_flow, curr_dir,
-                                                    shell_graph_path, shell_curr_dir)
-                create_ga_jobscript(k_value, net_load, num_flow, ksp_res_file, curr_dir,
-                                    shell_curr_dir)
+            # Create the KSP, Path Constrained LP & GA jobscripts
+            for k_value in args.k_values:
+                ksp_res_file = create_ksp_jobscript(k_value, net_load, num_flow, current_dir, shell_graph_path,
+                                                    shell_current_dir)
+
+                create_pc_lp_jobscript(net_load, num_flow, ksp_res_file, current_dir, shell_current_dir)
+
+                create_ga_jobscript(k_value, net_load, num_flow, ksp_res_file, current_dir, shell_current_dir)
 
 
 if __name__ == '__main__':
