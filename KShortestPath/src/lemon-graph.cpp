f0220c5ac9b184e75a0360341eb226723acc0991
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2019-02-07 13:14:08 +0100

    Refactor and restyle the LemonGraph class
    The LemonGraph class has been refactored to remove unnecessary functionality and move all the function implementations to the source file. The use of string stream to report an error has been removed and the std::to_string function is being used instead.

--- KShortestPath/src/lemon-graph.cpp
+++ KShortestPath/src/lemon-graph.cpp
@@ -8,103 +8,88 @@ LemonGraph::LemonGraph () : m_nodeLabel(m_graph), m_nodeType(m_graph),
                             m_linkCapacity(m_graph)
 {}
 
-DefLemonGraph::node_t
-LemonGraph::GetNode (identifier_t nodeId) const
-{
-  try
-    {
-      return m_nodeIdToNode.at (nodeId);
-    }
-  catch (std::out_of_range& ex)
-    {
-      std::stringstream ss;
-      ss << "Node with id: " << nodeId << " not found";
-      throw std::out_of_range(ss.str ());
+LemonGraph::node_t LemonGraph::GetNode (id_t nodeId) const {
+    try {
+        return m_nodeIdToNode.at(nodeId);
+    } catch (const std::out_of_range& ex) {
+        throw std::out_of_range("Node with id: " + std::to_string(nodeId) + " not found");
     }
 }
 
-DefLemonGraph::link_t
-LemonGraph::GetLink (identifier_t linkId) const
-{
-  try
-    {
-      return m_linkIdToLink.at (linkId);
-    }
-  catch (std::out_of_range& ex)
-    {
-      std::stringstream ss;
-      ss << "Link with id: " << linkId << " not found";
-      throw std::out_of_range(ss.str ());
+id_t LemonGraph::GetNodeId(LemonGraph::node_t node) const {
+    return m_nodeLabel[node];
+}
+
+char LemonGraph::GetNodeType(node_t node) const {
+    return m_nodeType[node];
+}
+
+LemonGraph::nodeIt_t LemonGraph::GetNodeIt() const {
+    return nodeIt_t(m_graph);
+}
+
+LemonGraph::link_t LemonGraph::GetLink(id_t linkId) const {
+    try {
+        return m_linkIdToLink.at (linkId);
+    } catch (const std::out_of_range& ex) {
+        throw std::out_of_range("Link with id: " + std::to_string(linkId) + "not found");
     }
 }
 
-uint32_t LemonGraph::get_num_switches()
-{
-    uint32_t num_switches {0};
-
-    for (auto const& node_entry: m_nodeIdToNode) {
-        auto& node = node_entry.second;
-        auto node_type = m_nodeType[node];
-        if (node_type == 'S')
-            num_switches++;
-    }
-
-    return num_switches;
+id_t LemonGraph::GetLinkId(LemonGraph::link_t link) const {
+    return m_linkLabel[link];
 }
 
-uint32_t LemonGraph::get_num_terminals()
-{
-    uint32_t num_terminals {0};
+linkCost_t LemonGraph::GetLinkCost(LemonGraph::link_t link) const {
+    return m_linkCost[link];
+}
 
-    for (auto const& node_entry: m_nodeIdToNode) {
-        auto& node = node_entry.second;
-        auto node_type = m_nodeType[node];
-        if (node_type == 'T')
-            num_terminals++;
-    }
+linkCapacity_t LemonGraph::GetLinkCapacity(link_t link) const {
+    return m_linkCapacity[link];
+}
 
-    return num_terminals;
+LemonGraph::linkIt_t LemonGraph::GetLinkIt() const {
+    return LemonGraph::linkIt_t(m_graph);
+}
+
+LemonGraph::node_t LemonGraph::GetSourceNode(link_t link) const {
+    return m_graph.source (link);
+}
+
+LemonGraph::node_t LemonGraph::GetDestinationNode(link_t link) const {
+    return m_graph.target (link);
 }
 
 /**
  * @brief Loads the LGF graph into the m_lGraph instance
  * @param lgfPath The path to the LGF file
  */
-void
-LemonGraph::LoadGraphFromFile(const std::string& lgfPath)
-{
-  try
-    {
-      lemon::digraphReader(m_graph, lgfPath)
-        .nodeMap ("label", m_nodeLabel)
-        .nodeMap ("coordinates", m_nodeCoordinates)
-        .nodeMap ("type", m_nodeType)
-        .arcMap ("label", m_linkLabel)
-        .arcMap ("delay", m_linkCost)
-        .arcMap ("capacity", m_linkCapacity)
-        .run();
-
-      LOG_MSG("Graph parsed successfully");
-
-      LOG_MSG("Building node id -> node map...");
-      for (nodeIt_t node = GetNodeIt (); node != lemon::INVALID; ++node)
-        {
-          identifier_t nodeId = m_nodeLabel[node];
-          m_nodeIdToNode.insert (std::make_pair(nodeId, node));
+void LemonGraph::LoadGraphFromFile(const std::string& lgfPath) {
+    try {
+        lemon::digraphReader(m_graph, lgfPath)
+            .nodeMap("label", m_nodeLabel)
+            .nodeMap("coordinates", m_nodeCoordinates)
+            .nodeMap("type", m_nodeType)
+            .arcMap("label", m_linkLabel)
+            .arcMap("delay", m_linkCost)
+            .arcMap("capacity", m_linkCapacity)
+            .run();
+        
+        LOG_MSG("Graph parsed successfully");
+        
+        LOG_MSG("Building node id -> node map...");
+        for (nodeIt_t node = GetNodeIt(); node != lemon::INVALID; ++node) {
+            identifier_t nodeId = m_nodeLabel[node];
+            m_nodeIdToNode.emplace(nodeId, node);
         }
-
-      LOG_MSG("Building link id -> link map...");
-      for (linkIt_t link = GetLinkIt (); link != lemon::INVALID; ++link)
-        {
-          identifier_t linkId = m_linkLabel[link];
-          m_linkIdToLink.insert (std::make_pair(linkId, link));
+        
+        LOG_MSG("Building link id -> link map...");
+        for (linkIt_t link = GetLinkIt(); link != lemon::INVALID; ++link) {
+            identifier_t linkId = m_linkLabel[link];
+            m_linkIdToLink.emplace(linkId, link);
         }
-    }
-  catch (lemon::Exception& e)
-    {
-      std::stringstream ss;
-      ss << "Error parsing the LGF graph\nLGF Location: "
-         << lgfPath << "\n" << "Error: " << e.what();
-      throw std::runtime_error(ss.str());
+    } catch (const lemon::Exception& e) {
+        throw std::runtime_error("Error parsing the LGS graph.\nLGF Location: " + lgfPath +
+                                 "\nError: " + e.what());
     }
 }
--- KShortestPath/src/lemon-graph.hpp
+++ KShortestPath/src/lemon-graph.hpp
@@ -2,60 +2,60 @@
 #define LEMONGRAPH_HPP
 
 #include <map>
+#include <lemon/dim2.h>
+#include <lemon/smart_graph.h>
 
 #include "definitions.hpp"
 
-using namespace DefLemonGraph;
-/**
- * @brief The LemonGraph class
- */
-class LemonGraph
-{
+class LemonGraph {
 public:
-  LemonGraph();
+    using graph_t = lemon::SmartDigraph;
+    /* Node definitions */
+    using node_t = graph_t::Node;
+    using nodeIt_t = graph_t::NodeIt;
+    using nodeLabelMap_t = graph_t::NodeMap<id_t>;
+    using nodeTypeMap_t = graph_t::NodeMap<char>;
+    using nodeCoordMap_t = graph_t::NodeMap<lemon::dim2::Point<int>>;
+    /* Link definitions */
+    using link_t = graph_t::Arc;
+    using linkIt_t = graph_t::ArcIt;
+    using linkLabelMap_t = graph_t::ArcMap<id_t>;
+    using linkCostMap_t = graph_t::ArcMap<linkCost_t>;
+    using linkCapacityMap_t = graph_t::ArcMap<linkCapacity_t>;
+    
+    LemonGraph();
 
-  /* Node related items*/
-  node_t GetNode (identifier_t nodeId) const; /*{ return m_nodeIdToNode.at(nodeId); }*/
-  identifier_t GetNodeId (node_t node) const { return m_nodeLabel[node]; }
-  char GetNodeType(node_t node) const { return m_nodeType[node]; }
-  nodeIt_t GetNodeIt () const { return nodeIt_t (m_graph); }
-  /* Link related items*/
-  link_t GetLink (identifier_t linkId) const;
-  identifier_t GetLinkId (link_t link) const { return m_linkLabel[link]; }
-  linkCost_t GetLinkCost (link_t link) const { return m_linkCost[link]; }
-  linkCapacity_t GetLinkCapacity (link_t link) const
-  {
-    return m_linkCapacity[link];
-  }
-  uint32_t GetNumLinks () const
-  {
-    return static_cast<uint32_t>(m_graph.arcNum ());
-  }
-  node_t GetSourceNode (link_t link) const { return m_graph.source (link); }
-  node_t GetDestinationNode (link_t link) const
-  {
-    return m_graph.target (link);
-  }
-  linkIt_t GetLinkIt () const { return linkIt_t (m_graph); }
-  graph_t& GetGraph() { return m_graph; }
-  uint32_t get_num_switches ();
-  uint32_t get_num_terminals ();
-
-  /* Functions */
-  void LoadGraphFromFile(const std::string& lgfPath);
-
+    /* Node getters */
+    node_t GetNode(id_t nodeId) const;
+    id_t GetNodeId(node_t node) const;
+    char GetNodeType(node_t node) const;
+    nodeIt_t GetNodeIt() const;
+    
+    /* Link getters */
+    link_t GetLink(id_t linkId) const;
+    id_t GetLinkId(link_t link) const;
+    linkCost_t GetLinkCost(link_t link) const;
+    linkCapacity_t GetLinkCapacity(link_t link) const;
+    linkIt_t GetLinkIt() const;
+    node_t GetSourceNode(link_t link) const;
+    node_t GetDestinationNode(link_t link) const;
+    
+    void LoadGraphFromFile(const std::string& lgfPath);
+    
 private:
-  graph_t m_graph;
-  /* Node Maps */
-  nodeLabelMap_t m_nodeLabel;
-  nodeTypeMap_t m_nodeType;
-  nodeCoordMap_t m_nodeCoordinates;
-  std::map<identifier_t, node_t> m_nodeIdToNode;
-  /* Link Maps */
-  linkLabelMap_t m_linkLabel;
-  linkCostMap_t m_linkCost;
-  linkCapacityMap_t m_linkCapacity;
-  std::map<identifier_t, link_t> m_linkIdToLink;
+    graph_t m_graph;
+    
+    /* Node Maps */
+    nodeLabelMap_t m_nodeLabel;
+    nodeTypeMap_t m_nodeType;
+    nodeCoordMap_t m_nodeCoordinates;
+    std::map<id_t, node_t> m_nodeIdToNode;
+    
+    /* Link Maps */
+    linkLabelMap_t m_linkLabel;
+    linkCostMap_t m_linkCost;
+    linkCapacityMap_t m_linkCapacity;
+    std::map<id_t, link_t> m_linkIdToLink;
 };
 
 #endif // LEMONGRAPH_HPP
