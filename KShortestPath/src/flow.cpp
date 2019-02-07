13ba62547848639c441bd64dcdbad63b8827a276
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2019-02-07 17:24:20 +0100

    Refactor the Path and Flow classes
    Flow parsing has been updated to remove unnecessary details that are no longer required by the ns3 simulation such as the port numbers. The Path and Flow class have all been updated to make sole use of the Boost Graph functionality.

--- KShortestPath/src/flow.cpp
+++ KShortestPath/src/flow.cpp
@@ -4,170 +4,266 @@
 
 #include "flow.hpp"
 
-Flow::Flow(const std::string &line) : srcPort (0)
-{
-  Parse(line);
-}
+/**
+ Path implementation
+ */
 
-Flow&
-Flow::operator=(const Flow &rhs)
-{
-  if (&rhs == this) return *this; // Check for self-assignment
+//Path::Path(std::pair<linkCost_t, std::list<DefBoostGraph::link_t>> path,
+//           const std::map<DefBoostGraph::link_t,
+//                          DefLemonGraph::link_t>& m_blLinkMap) :
+//    pathCost (path.first)
+//{
+//  for (auto& link : path.second) // Populate list of links the path uses
+//    links.emplace_back (m_blLinkMap.at (link));
+//}
 
-  id = rhs.id;
-  sourceId = rhs.sourceId;
-  destinationId = rhs.destinationId;
-  dstPort = rhs.dstPort;
-  dataRate = rhs.dataRate;
-  packetSize = rhs.packetSize;
-  numOfPackets = rhs.numOfPackets;
-  protocol = rhs.protocol;
-  startTime = rhs.startTime;
-  endTime = rhs.endTime;
-  tcpFlowId = rhs.tcpFlowId;
+///**
+// * @brief Overloads the Path::operator = to check if to Path objects are equal
+// */
+//Path&
+//Path::operator= ( const Path& rhs )
+//{
+//  if (&rhs == this) return *this; // Check for self-assignment
+//
+//  pathCost = rhs.pathCost;
+//  links = rhs.links;
+//  return *this;
+//}
 
-  return *this;
-}
+id_t Path::globalPathId = 0;
 
-std::ostream&
-operator<< (std::ostream& output, const Flow& flow)
-{
-  output << "Id: " << flow.id
-         << " Source: " <<  flow.GetSourceId()
-         << " Destination: " << flow.GetDestinationId() << "\n"
-         << "Dst Port Number: " << flow.dstPort;
-
-  output << " Data Rate: " << flow.dataRate << "Mbps\n"
-         << "Packet Size: " << flow.packetSize << "bytes Num Of Packets: "
-         << flow.numOfPackets << " Protocol: "
-         << static_cast<std::underlying_type<Protocol>::type>(flow.protocol)
-         << "\n" << "Start Time: " << flow.startTime
-         << "s End Time: " << flow.endTime << "s\n";
-
-  if (flow.protocol == Protocol::Ack)
-    output << "TCP Data Flow ID: " << flow.tcpFlowId;
-
-  return output;
-}
-
-void
-Flow::Parse(const std::string &line)
-{
-  std::istringstream flowSs (line, std::istringstream::in);
-
-  try
-    {
-      flowSs >> id >> sourceId >> destinationId >> dstPort >> dataRate
-             >> packetSize >> numOfPackets;
-
-      char parsedProtocol;
-      flowSs >> parsedProtocol;
-
-      if (parsedProtocol == 'T') {
-        protocol = Protocol::Tcp;
-      } else if (parsedProtocol == 'U') {
-        protocol = Protocol::Udp;
-      } else if (parsedProtocol == 'A') {
-        protocol = Protocol::Ack;
-      } else {
-        throw std::invalid_argument ("Unknown protocol type");
-      }
-
-      flowSs >> startTime >> endTime;
-
-      if (protocol == Protocol::Ack) { // Retrieve the TCP Flow Id ONLY if it is an ACK flow
-        flowSs >> tcpFlowId;
-      }
-    }
-  catch (std::invalid_argument& e)
-    {
-      std::cerr << "Error when converting flow values OR Invalid Protocol Type\n"
-      << e.what() << std::endl;
-      throw;
-    }
-  catch (std::out_of_range& e)
-    {
-      std::cerr << "Value out of range\n" << e.what() << std::endl;
-      throw;
+Path::Path(bool setPathId) {
+    if (setPathId) {
+        id = globalPathId++;
     }
 }
 
-void SetFileCursorToFlowsSection (std::ifstream& file);
-
-Flow::flowContainer_t
-ParseFlows (const std::string& lgfPath)
-{
-  std::ifstream lgfFile;
-  lgfFile.exceptions(std::ifstream::badbit); // Enable exceptions if error occurs during file read
-
-  Flow::flowContainer_t flows;
-
-  try {
-    LOG_MSG("Loading flows from " + lgfPath);
-    lgfFile.open(lgfPath, std::ifstream::in); // Open file as Read Only
-
-    SetFileCursorToFlowsSection(lgfFile); // Set the file cursor to the flows section
-
-    std::string line;
-    while (std::getline(lgfFile, line)) {
-        if (!line.empty()) {
-          Flow flow(line);
-          auto ret = flows.emplace (std::make_pair (flow.GetFlowId (), flow));
-          if (!ret.second) {
-            std::cerr << "ERROR: Trying to insert a duplicate flow. Flow Id: " << flow.GetFlowId ();
-          }
-        }
-    }
-  } catch (std::ifstream::failure& e) {
-    std::cerr << "Loading the LGF file failed\n" << lgfPath << "\n"
-              << e.what()  << std::endl;
-    throw;
-  }
-
-  return flows;
+void Path::AddLink(id_t linkId) {
+    m_links.emplace_back(linkId);
 }
 
 /**
- Sets the cursor in the file passed as parameter to this
- function to the @flow section while ignoring any comments
- that begin with the '#' character.
-
- @param file Reference to the lgf file.
+ * @brief Overloads the Path::operator < to compare to paths. Used for sorting
  */
-void
-SetFileCursorToFlowsSection(std::ifstream& file)
-{
-  std::string line;
-  std::string flowSectionString ("@flows");
-  bool flowsSectionFound (false);
+bool Path::operator<(const Path& other) const {
+    return cost < other.cost;
+}
 
-  int lineNumber (1);
-  while (std::getline (file, line))
-    {
-      lineNumber++;
+std::ostream& operator<< (std::ostream& output, const Path& path) {
+    output << "  Path ID: " << path.id << "\n"
+           << "  Path Cost: " << path.cost << "\n"
+           << "  Links: ";
+    
+    for (const auto& link : path.m_links) {
+        output << link << " ";
+    }
+    
+    return output;
+}
+//std::string Path::str (const LemonGraph &lemonGraph)
+//{
+//  std::stringstream ss;
+//
+//  ss << "Path Cost: " << pathCost << "\n"
+//     << "Path: ";
+//
+//  for (auto& link : links)
+//    ss << lemonGraph.GetLinkId (link) << " ";
+//
+//  return ss.str();
+//}
 
-      // Ignore any comments after the @flows section
-      if (flowsSectionFound && line[0] != '#')
-        break; // Line found
+/**
+ Flow implementation
+ */
 
-      // Ignore any trailing white space in the file
-      if (flowSectionString.compare(0, flowSectionString.length(), line,
-                                    0, flowSectionString.length()) == 0)
-        {
-          flowsSectionFound = true;
+Flow::Flow(const std::string &line) {
+  Parse(line);
+}
+
+const std::list<Path>& Flow::GetDataPaths () const {
+    return m_dataPaths;
+}
+
+const std::list<Path>& Flow::GetAckPaths() const {
+    return m_ackPaths;
+}
+
+void Flow::AddDataPath (const Path& path) {
+    m_dataPaths.emplace_back(path);
+}
+
+void Flow::AddAckPath(const Path &path) {
+    m_ackPaths.emplace_back(path);
+}
+
+//Flow&
+//Flow::operator=(const Flow &rhs)
+//{
+//  if (&rhs == this) return *this; // Check for self-assignment
+//
+//  id = rhs.id;
+//  sourceId = rhs.sourceId;
+//  destinationId = rhs.destinationId;
+//  dstPort = rhs.dstPort;
+//  dataRate = rhs.dataRate;
+//  packetSize = rhs.packetSize;
+//  numOfPackets = rhs.numOfPackets;
+//  protocol = rhs.protocol;
+//  startTime = rhs.startTime;
+//  endTime = rhs.endTime;
+//  tcpFlowId = rhs.tcpFlowId;
+//
+//  return *this;
+//}
+
+bool Flow::operator<(const Flow &other) const {
+    return id < other.id;
+}
+
+void Flow::Parse(const std::string &line) {
+    
+    std::istringstream flowSs (line, std::istringstream::in);
+    try {
+        auto tempDstPort{0}; // Port number is no longer needed; thus, it is ignored
+        flowSs >> id >> sourceId >> destinationId >> tempDstPort >> dataRate
+               >> packetSize >> numOfPackets;
+        
+        char parsedProtocol;
+        flowSs >> parsedProtocol;
+        
+        if (parsedProtocol == 'T') {
+            protocol = Protocol::Tcp;
+        } else if (parsedProtocol == 'U') {
+            protocol = Protocol::Udp;
+        } else if (parsedProtocol == 'A') {
+            protocol = Protocol::Ack;
+        } else {
+            throw std::invalid_argument ("Unknown protocol type");
+        }
+        
+        flowSs >> startTime >> endTime;
+    } catch (const std::invalid_argument& ia) {
+        std::cerr << "Error when converting flow values OR Invalid Protocol Type\n"
+                  << ia.what() << std::endl;
+        throw;
+    } catch (const std::out_of_range& oor) {
+        std::cerr << "Value out of range\n" << oor.what() << std::endl;
+        throw;
+    }
+}
+
+std::ostream& operator<< (std::ostream& output, const Flow& flow) {
+    output << "----------\n"
+    << "Id: " << flow.id << "\n"
+    << " Source: " <<  flow.sourceId << "\n"
+    << " Destination: " << flow.destinationId << "\n"
+    << " Data Rate: " << flow.dataRate << "Mbps\n"
+    << " Packet Size: " << flow.packetSize << "bytes\n"
+    << " Number of packets: " << flow.numOfPackets << "\n"
+    << " Protocol: " << static_cast<char>(flow.protocol) << "\n"
+    << " Start Time: " << flow.startTime << "s\n"
+    << " End Time: " << flow.endTime << "s\n"
+    << "----------\n";
+
+    if (!flow.m_dataPaths.empty()) {
+        output << "Data Paths\n"
+               << "----------\n";
+        for (const auto& path : flow.m_dataPaths) {
+            output << path << "\n----------\n";
         }
     }
 
-  if (!flowsSectionFound)
-    throw std::invalid_argument ("Flow section not found");
+    if (!flow.m_ackPaths.empty()) {
+        output << "Ack Paths\n"
+               << "----------\n";
+        for (const auto& path : flow.m_ackPaths) {
+            output << path << "\n----------\n";
+        }
+    }
 
-  // Move to the beginning of the file
-  file.seekg(std::ios::beg);
+    return output;
+}
 
-  // Move one line up to where the flow definitions begins
-  for (int i = 1; i < (lineNumber - 1); i++)
-    {
-      // Extract and ignore a line
-      file.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+/**
+ Sets the cursor in the file passed as parameter to the '@flow' section
+ while ignoring any comments that begin with the '#' character.
+
+ @param file Reference to the lgf file stream.
+ */
+void SetFileCursorToFlowsSection(std::ifstream& file) {
+    std::string line;
+    std::string flowSectionString ("@flows");
+    bool flowsSectionFound (false);
+    
+    int lineNumber (1);
+    while (std::getline (file, line)) {
+        lineNumber++;
+        
+        // Ignore any comments after the @flows section
+        if (flowsSectionFound && line[0] != '#') {
+            break; // Line found
+        }
+        
+        // Ignore any trailing white space in the file
+        if (flowSectionString.compare(0, flowSectionString.length(), line,
+                                      0, flowSectionString.length()) == 0) {
+            flowsSectionFound = true;
+        }
+    }
+    
+    if (!flowsSectionFound) {
+        throw std::invalid_argument ("Flow section not found");
+    }
+    
+    // Move to the beginning of the file
+    file.seekg(std::ios::beg);
+    
+    // Move one line up to where the flow definitions begins
+    for (int i = 1; i < (lineNumber - 1); i++) {
+        file.ignore(std::numeric_limits<std::streamsize>::max(),'\n'); // Extract and ignore a line
+    }
+}
+
+
+Flow::flowContainer_t ParseFlows (const std::string& lgfPath) {
+    Flow::flowContainer_t flows;
+    
+    try {
+        std::ifstream lgfFile;
+        lgfFile.exceptions(std::ifstream::badbit); // Enable exceptions if error occurs during file read
+        lgfFile.open(lgfPath, std::ifstream::in); // Open file as Read Only
+        SetFileCursorToFlowsSection(lgfFile); // Set the file cursor to the flows section
+        
+        LOG_MSG("File read successfully. Loading flows from " + lgfPath);
+        
+        std::string line;
+        while (std::getline(lgfFile, line)) {
+            if (!line.empty()) {
+                Flow flow(line);
+                
+                if (flow.protocol == Protocol::Ack) { // Ignore Acknowledgement flows
+                    continue;
+                }
+                
+                auto ret = flows.emplace(flow.id, flow);
+                if (!ret.second) {
+                    std::cerr << "ERROR: Trying to insert a duplicate flow. Flow Id: " << flow.id;
+                }
+                LOG_MSG(flow);
+            }
+        }
+    } catch (const std::ifstream::failure& e) {
+        std::cerr << "Loading the LGF file failed\n" << lgfPath << "\n"
+        << e.what()  << std::endl;
+        throw;
+    }
+    
+    return flows;
+}
+
+void PrintFlows (const Flow::flowContainer_t& flows) {
+    for (const auto& flowPair : flows) {
+        std::cout << flowPair.second;
     }
 }
--- KShortestPath/src/flow.hpp
+++ KShortestPath/src/flow.hpp
@@ -1,69 +1,71 @@
 #ifndef flow_parser_hpp
 #define flow_parser_hpp
 
+#include <map>
+#include <list>
 #include <string>
 #include <fstream>
-#include <map>
 
 #include "definitions.hpp"
-#include "path.hpp"
 
-enum class Protocol : char { Tcp = 'T', Ack = 'A', Udp = 'U'};
+enum class Protocol : char {Tcp='T', Ack='A', Udp='U', Undefined='X'};
 
-class Flow
-{
-public:
-  using flowContainer_t = std::map<identifier_t, Flow>;
-  using port_t = uint64_t;
+struct Path {
+    static id_t globalPathId;
+    
+    id_t id {0};
+    linkCost_t cost {0};
+    
+    Path(bool setPathId);
+    
+    void AddLink (id_t linkId);
+    bool operator< (const Path& other) const;
+    
+    friend std::ostream& operator<< (std::ostream& output, const Path& path);
+//    std::string str(const LemonGraph& lemonGraph);
+//    Path(std::pair<linkCost_t, std::list<DefBoostGraph::link_t>> path,
+//         const std::map<DefBoostGraph::link_t,
+//         DefLemonGraph::link_t>& m_blLinkMap);
 
-  Flow() = delete;
-  explicit Flow(const std::string &line);
+//    Path& operator= (const Path& rhs);
+private:
+    std::list<id_t> m_links; /**< List of link ids the path goes through. */
+};
 
-  // Setters
-  void SetSrcPort(port_t srcPort) { this->srcPort = srcPort; }
-
-  // Getters
-  identifier_t GetFlowId () const { return id; }
-  identifier_t GetSourceId () const { return sourceId; }
-  identifier_t GetDestinationId () const { return destinationId; }
-  double GetDataRate () const { return dataRate; }
-  port_t GetSrcPort () const { return srcPort; }
-  port_t GetDstPort () const { return dstPort; }
-  uint64_t GetPktSize () const { return packetSize; }
-  uint64_t GetNumOfPackets () const { return numOfPackets; }
-  Protocol GetProtocol () const { return protocol; }
-  uint64_t GetStartTime () const { return startTime; }
-  uint64_t GetEndTime () const { return endTime; }
-  identifier_t GetTcpFlowId () const { return tcpFlowId; }
-  const std::vector<Path>& GetPaths () const { return m_paths; }
-
-  void AddPath (const Path& path) { m_paths.emplace_back (path); }
-
-  Flow& operator= ( const Flow& rhs );
-
-  bool operator < (const Flow& other) const { return id < other.id; }
-  friend std::ostream& operator<< (std::ostream& output, const Flow& flow);
+struct Flow {
+    using flowContainer_t = std::map<identifier_t, Flow>;
+    using dataRate_t = double;
+    using port_t = uint16_t;
+    
+    id_t id{0};                     // Flow Id
+    id_t sourceId{0};               // Source Node Id
+    id_t destinationId{0};          // Destination Node Id
+    dataRate_t dataRate{0.0};               // The flow's data rate including headers
+    uint64_t packetSize{0};                 // The flow's packet size including headers
+    uint64_t numOfPackets{0};               // Number of packets the flow is to transmit
+    Protocol protocol{Protocol::Undefined}; // The protocol the flow can use
+    uint64_t startTime{0};                  // Time when to start transmission
+    uint64_t endTime{0};                    // Time when to stop transmission
+//    identifier_t tcpFlowId;     // This is the flow ID of the TCP data stream.
+//                                // This is only used by ACK flows to determine
+//                                // to which data flow this ACK flow belongs to.
+    
+    Flow() = delete;
+    explicit Flow(const std::string &line);
+    
+    const std::list<Path>& GetDataPaths() const;
+    const std::list<Path>& GetAckPaths() const;
+    void AddDataPath(const Path& path);
+    void AddAckPath(const Path& path);
+    
+//    Flow& operator= ( const Flow& rhs );
+    bool operator< (const Flow& other) const;
+    friend std::ostream& operator<< (std::ostream& output, const Flow& flow);
 
 private:
-  void Parse (const std::string& line);
-
-  // Flow details
-  identifier_t id;            // Flow Id
-  identifier_t sourceId;      // Source Node Id
-  identifier_t destinationId; // Destination Node Id
-  port_t srcPort;             // Source Port
-  port_t dstPort;             // Destination Port
-  double dataRate;            // The flow's data rate including headers
-  uint64_t packetSize;        // The flow's packet size including headers
-  uint64_t numOfPackets;      // Number of packets the flow is to transmit
-  Protocol protocol;          // The protocol the flow can use
-  uint64_t startTime;         // Time when to start transmission
-  uint64_t endTime;           // Time when to stop transmission
-  identifier_t tcpFlowId;     // This is the flow ID of the TCP data stream.
-                              // This is only used by ACK flows to determine
-                              // to which data flow this ACK flow belongs to.
-
-  std::vector<Path> m_paths;   // List of paths the flow uses
+    void Parse (const std::string& line);
+    std::list<Path> m_dataPaths; /**< List of paths the data flow will use */
+    std::list<Path> m_ackPaths; /**< List of paths the ack flow will use */
 };
 
 /**
@@ -73,4 +75,6 @@ private:
  */
 Flow::flowContainer_t ParseFlows (const std::string& lgfPath);
 
+void PrintFlows (const Flow::flowContainer_t& flows);
+    
 #endif /* flow_parser_hpp */
