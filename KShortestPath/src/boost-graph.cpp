235c96459dd73f81d77788de4c62ba4a2ef84795
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2019-02-07 13:10:56 +0100

    Refactor the BoostGraph class
    The BoostGraph class has been refactored such that once converted, all further operations can be carried out using solely the boost graph. NOTE: The KSP algorithm has yet to be updated to conform with this new graph.

--- KShortestPath/src/boost-graph.cpp
+++ KShortestPath/src/boost-graph.cpp
@@ -1,199 +1,175 @@
 #include <boost/numeric/conversion/cast.hpp>
-#include "boost-graph.hpp"
+
 #include "yen_ksp.hpp"
+#include "boost-graph.hpp"
 
-BoostGraph::BoostGraph (const LemonGraph& lemonGraph): m_lemonGraph(lemonGraph)
-{}
-
-/**
- * @brief Returns the boost graph's lemon node equivalent
- * @param lemonNode The node in the Lemon graph
- * @return The Boost node represented by the lemon's graph lemon node lemonNode
- */
-DefBoostGraph::node_t
-BoostGraph::GetNode(DefLemonGraph::node_t lemonNode)
-{
-  try
-    {
-      return m_lbNodeMap.at (lemonNode);
-    }
-  catch (std::out_of_range&)
-    {
-      throw std::out_of_range ("Lemon node not found in boost graph\n");
-    }
+BoostGraph::BoostGraph (const LemonGraph& lemonGraph) {
+    GenerateBoostGraph(lemonGraph);
 }
 
 /**
  * @brief Generate a boost graph equivalent of the lemon graph
  * @param lemonGraph LemonGraph instance that contains the parsed lemon graph
  */
-void
-BoostGraph::GenerateBoostGraphFromLemonGraph(const LemonGraph& lemonGraph)
-{
-  LOG_MSG("Building nodes...");
-
-  for (DefLemonGraph::nodeIt_t lemonNode = lemonGraph.GetNodeIt ();
-       lemonNode != lemon::INVALID;
-       ++lemonNode)
-    {
-      DefBoostGraph::node_t node = boost::add_vertex(m_graph);
-
-      auto ret = m_lbNodeMap.insert(std::make_pair(lemonNode, node));
-      if (!ret.second)
-        {
-          std::stringstream ss;
-          ss << "Trying to insert a duplicate node. Node Id: "
-             << lemonGraph.GetNodeId (lemonNode) << "\n";
-          throw std::runtime_error(ss.str());
-        }
-    }
-
-  LOG_MSG("Building links...");
-
-  for (DefLemonGraph::linkIt_t lemonLink = lemonGraph.GetLinkIt ();
-       lemonLink != lemon::INVALID;
-       ++lemonLink)
-    {
-      DefLemonGraph::node_t lemonSrcNode = lemonGraph.GetSourceNode (lemonLink);
-      DefLemonGraph::node_t lemonDstNode =
-          lemonGraph.GetDestinationNode (lemonLink);
-
-      // Get boost source and destination nodes
-      DefBoostGraph::node_t srcNode = GetNode (lemonSrcNode);
-      DefBoostGraph::node_t dstNode = GetNode (lemonDstNode);
-
-      // Create link
-      DefBoostGraph::link_t link;
-      bool linkAdded;
-      boost::tie (link, linkAdded) = boost::add_edge(srcNode, dstNode, m_graph);
-
-      if (!linkAdded)
-        {
-          std::stringstream ss;
-          ss << "Link could not be added in Boost graph. Link Id: "
-             << lemonGraph.GetLinkId (lemonLink) << "\n";
-          throw std::runtime_error(ss.str());
-        }
-
-      // Set the cost of the link
-      boost::get(boost::edge_weight, m_graph, link) =
-          lemonGraph.GetLinkCost (lemonLink);
-
-      // Add boost -> lemon link map
-      auto ret = m_blLinkMap.insert(std::make_pair(link, lemonLink));
-      if (!ret.second)
-        {
-          std::stringstream ss;
-          ss << "Trying to insert a duplicate link. Link Id: "
-             << lemonGraph.GetLinkId (lemonLink) << "\n";
-          throw std::runtime_error(ss.str());
-        }
-    }
-
-  LOG_MSG("LEMON Graph converted to BOOST Graph successfully");
+void BoostGraph::GenerateBoostGraph(const LemonGraph& lemonGraph) {
+    GenerateBoostNodes(lemonGraph);
+    GenerateBoostLinks(lemonGraph);
+    LOG_MSG("LEMON Graph converted to BOOST Graph successfully");
 }
 
-bool numbersAreClose(linkCapacity_t value1, linkCapacity_t value2,
-                     linkCapacity_t accuracy=1e-9)
-{
-  return (std::fabs (value1 - value2) < accuracy);
-}
-
-/**
- * @brief Add the kth shortest paths for each data flow including paths with
- * same cost as path k
- *
- * Add the kth shortest paths for each data flow including paths with same
- * cost as path k. Data flows refer to either TCP or UDP flows. Acknowledgement
- * flows are routed over the single shortest path (i.e. k = 1).
- *
- * @param flows List of flows that will be updated with the paths
- * @param k The number of paths to include for each flow
- */
-void
-BoostGraph::AddKShortestPaths(Flow::flowContainer_t& flows, uint32_t k)
-{
-  for (auto& flowPair: flows)
-    {
-      Flow &flow = flowPair.second;
-
-      DefBoostGraph::node_t srcNode =
-        GetNode (m_lemonGraph.GetNode (flow.GetSourceId ()));
-      DefBoostGraph::node_t dstNode =
-          GetNode (m_lemonGraph.GetNode (flow.GetDestinationId ()));
-
-      std::list<std::pair<linkCost_t,
-                          std::list<DefBoostGraph::link_t>>> kShortestPaths;
-
-      if (flow.GetProtocol () == Protocol::Ack)  // Route Ack flows
-        {
-          kShortestPaths = boost::yen_ksp (m_graph, srcNode, dstNode, 1);
-
-          if (kShortestPaths.empty ())
-            throw std::runtime_error ("ACK flow route not found");
+void BoostGraph::GenerateBoostNodes(const LemonGraph& lemonGraph) {
+    LOG_MSG("Building nodes...");
+    for (auto lemonNode = lemonGraph.GetNodeIt(); lemonNode != lemon::INVALID; ++lemonNode) {
+        auto nodeId {lemonGraph.GetNodeId(lemonNode)};
+        auto nodeType {lemonGraph.GetNodeType(lemonNode)};
+        
+        auto boostNode {boost::add_vertex({nodeId, nodeType}, m_graph)};
+        auto ret = m_nodeMap.emplace(nodeId, boostNode);
+        if (!ret.second) {
+            throw std::runtime_error("Trying to insert a duplicate node. Node Id: " +
+                                     std::to_string(nodeId) + "\n");
         }
-      else // Route data flows
-        {
-          uint64_t numPathsToInclude {0};
-          uint32_t increasedK{k};
-          std::list<DefBoostGraph::link_t> prevLastPathLinks;
-
-          while (true)
-            {
-              increasedK =
-                boost::numeric_cast<uint32_t> (std::lround (increasedK * 1.5));
-
-              kShortestPaths = boost::yen_ksp (m_graph, srcNode, dstNode,
-                                               increasedK);
-
-              if (kShortestPaths.size () < k)
-                { // Number of paths found less than requested
-                  numPathsToInclude = kShortestPaths.size ();
-                  break;
-                }
-
-              auto& lastPath = kShortestPaths.back ();
-
-              if (lastPath.second == prevLastPathLinks) // No new paths for flow
-                {
-                  numPathsToInclude = kShortestPaths.size();
-                  break;
-                }
-              else
-                {
-                  auto kthPath = kShortestPaths.begin ();
-                  std::advance (kthPath, k - 1);
-                  linkCost_t kthPathCost{kthPath->first};
-
-                  numPathsToInclude = k;
-
-                  for (auto& currentPath = ++kthPath; // Start from path k+1
-                       currentPath != kShortestPaths.end ();
-                       ++currentPath)
-                    {
-                      if (numbersAreClose (kthPathCost, currentPath->first))
-                        numPathsToInclude++;
-                    }
-
-                  if (numPathsToInclude < kShortestPaths.size())
-                    break;
-                  else if (numPathsToInclude > kShortestPaths.size())
-                    {
-                      std::stringstream ss;
-                      ss << "Error when finding paths for flow: "
-                         << flow.GetFlowId ();
-                      throw std::runtime_error(ss.str ());
-                    }
-                }
-              prevLastPathLinks = std::move(kShortestPaths.back().second);
-            }
-
-          auto firstPathToRemove = kShortestPaths.begin();
-          std::advance (firstPathToRemove, numPathsToInclude);
-          kShortestPaths.erase(firstPathToRemove, kShortestPaths.end ());
-        }
-
-      for (auto& path: kShortestPaths) // Add all the paths to the flow
-          flow.AddPath(Path(path, m_blLinkMap));
+        
+        LOG_MSG("Added node " << boost::get(&NodeDetails::id, m_graph, boostNode) <<
+                " Type " << boost::get(&NodeDetails::type, m_graph, boostNode));
     }
 }
+
+void BoostGraph::GenerateBoostLinks(const LemonGraph& lemonGraph) {
+    LOG_MSG("Building links...");
+    for (auto lemonLink = lemonGraph.GetLinkIt(); lemonLink != lemon::INVALID; ++lemonLink) {
+        auto srcNodeId = lemonGraph.GetNodeId(lemonGraph.GetSourceNode(lemonLink));
+        auto dstNodeId = lemonGraph.GetNodeId(lemonGraph.GetDestinationNode(lemonLink));
+        
+        auto linkId = lemonGraph.GetLinkId(lemonLink);
+        auto linkCost = lemonGraph.GetLinkCost(lemonLink);
+        auto linkCapacity = lemonGraph.GetLinkCapacity(lemonLink);
+        
+        link_t boostLink;
+        bool linkAdded {false};
+        std::tie(boostLink, linkAdded) = boost::add_edge(m_nodeMap.at(srcNodeId),
+                                                         m_nodeMap.at(dstNodeId),
+                                                         {linkId, linkCost, linkCapacity},
+                                                         m_graph);
+        
+        if (!linkAdded) {
+            throw std::runtime_error("Link could not be added in Boost graph. Link Id: "
+                                     + std::to_string(linkId) + "\n");
+        }
+        
+        auto srcNode {boost::source(boostLink, m_graph)};
+        auto dstNode {boost::target(boostLink, m_graph)};
+        
+        LOG_MSG("Added link " << boost::get(&LinkDetails::id, m_graph, boostLink) <<
+                " Cost " << boost::get(&LinkDetails::cost, m_graph, boostLink) <<
+                " Capacity " << boost::get(&LinkDetails::capacity, m_graph, boostLink) <<
+                " Source Node " << boost::get(&NodeDetails::id, m_graph, srcNode) <<
+                " Destination Node " << boost::get(&NodeDetails::id, m_graph, dstNode));
+    }
+}
+
+//
+//bool numbersAreClose(linkCapacity_t value1, linkCapacity_t value2,
+//                     linkCapacity_t accuracy=1e-9)
+//{
+//  return (std::fabs (value1 - value2) < accuracy);
+//}
+//
+///**
+// * @brief Add the kth shortest paths for each data flow including paths with
+// * same cost as path k
+// *
+// * Add the kth shortest paths for each data flow including paths with same
+// * cost as path k. Data flows refer to either TCP or UDP flows. Acknowledgement
+// * flows are routed over the single shortest path (i.e. k = 1).
+// *
+// * @param flows List of flows that will be updated with the paths
+// * @param k The number of paths to include for each flow
+// */
+//void
+//BoostGraph::AddKShortestPaths(Flow::flowContainer_t& flows, uint32_t k)
+//{
+//  for (auto& flowPair: flows)
+//    {
+//      Flow &flow = flowPair.second;
+//
+//      DefBoostGraph::node_t srcNode =
+//        GetNode (m_lemonGraph.GetNode (flow.GetSourceId ()));
+//      DefBoostGraph::node_t dstNode =
+//          GetNode (m_lemonGraph.GetNode (flow.GetDestinationId ()));
+//
+//      std::list<std::pair<linkCost_t,
+//                          std::list<DefBoostGraph::link_t>>> kShortestPaths;
+//
+//      if (flow.GetProtocol () == Protocol::Ack)  // Route Ack flows
+//        {
+//          kShortestPaths = boost::yen_ksp (m_graph, srcNode, dstNode, 1);
+//
+//          if (kShortestPaths.empty ())
+//            throw std::runtime_error ("ACK flow route not found");
+//        }
+//      else // Route data flows
+//        {
+//          uint64_t numPathsToInclude {0};
+//          uint32_t increasedK{k};
+//          std::list<DefBoostGraph::link_t> prevLastPathLinks;
+//
+//          while (true)
+//            {
+//              increasedK =
+//                boost::numeric_cast<uint32_t> (std::lround (increasedK * 1.5));
+//
+//              kShortestPaths = boost::yen_ksp (m_graph, srcNode, dstNode,
+//                                               increasedK);
+//
+//              if (kShortestPaths.size () < k)
+//                { // Number of paths found less than requested
+//                  numPathsToInclude = kShortestPaths.size ();
+//                  break;
+//                }
+//
+//              auto& lastPath = kShortestPaths.back ();
+//
+//              if (lastPath.second == prevLastPathLinks) // No new paths for flow
+//                {
+//                  numPathsToInclude = kShortestPaths.size();
+//                  break;
+//                }
+//              else
+//                {
+//                  auto kthPath = kShortestPaths.begin ();
+//                  std::advance (kthPath, k - 1);
+//                  linkCost_t kthPathCost{kthPath->first};
+//
+//                  numPathsToInclude = k;
+//
+//                  for (auto& currentPath = ++kthPath; // Start from path k+1
+//                       currentPath != kShortestPaths.end ();
+//                       ++currentPath)
+//                    {
+//                      if (numbersAreClose (kthPathCost, currentPath->first))
+//                        numPathsToInclude++;
+//                    }
+//
+//                  if (numPathsToInclude < kShortestPaths.size())
+//                    break;
+//                  else if (numPathsToInclude > kShortestPaths.size())
+//                    {
+//                      std::stringstream ss;
+//                      ss << "Error when finding paths for flow: "
+//                         << flow.GetFlowId ();
+//                      throw std::runtime_error(ss.str ());
+//                    }
+//                }
+//              prevLastPathLinks = std::move(kShortestPaths.back().second);
+//            }
+//
+//          auto firstPathToRemove = kShortestPaths.begin();
+//          std::advance (firstPathToRemove, numPathsToInclude);
+//          kShortestPaths.erase(firstPathToRemove, kShortestPaths.end ());
+//        }
+//
+//      for (auto& path: kShortestPaths) // Add all the paths to the flow
+//          flow.AddPath(Path(path, m_blLinkMap));
+//    }
+//}
--- KShortestPath/src/boost-graph.hpp
+++ KShortestPath/src/boost-graph.hpp
@@ -1,29 +1,41 @@
-#ifndef BOOSTGRAPH_HPP
-#define BOOSTGRAPH_HPP
+#ifndef boostgraph_hpp
+#define boostgraph_hpp
 
 #include <map>
+#include <boost/graph/adjacency_list.hpp>
 
+#include "flow.hpp"
 #include "definitions.hpp"
 #include "lemon-graph.hpp"
-#include "flow.hpp"
 
-class BoostGraph
-{
-public:
-  explicit BoostGraph (const LemonGraph& lemonGraph);
-
-  DefBoostGraph::node_t GetNode (DefLemonGraph::node_t lemonNode);
-
-  void GenerateBoostGraphFromLemonGraph (const LemonGraph& lemonGraph);
-  void AddKShortestPaths (Flow::flowContainer_t &flows, uint32_t k);
-
-private:
-  DefBoostGraph::graph_t m_graph;
-  // Mapping between Boost <-> Lemon
-  std::map<DefLemonGraph::node_t, DefBoostGraph::node_t> m_lbNodeMap;
-  std::map<DefBoostGraph::link_t, DefLemonGraph::link_t> m_blLinkMap;
-
-  const LemonGraph& m_lemonGraph;
+struct NodeDetails {
+    id_t id;
+    char type; /* S = Switch | T = Terminal */
 };
 
-#endif /* BOOSTGRAPH_HPP */
+struct LinkDetails {
+    id_t id;
+    linkCost_t cost;
+    linkCapacity_t capacity;
+};
+
+class BoostGraph {
+public:
+    using graph_t = boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS,
+                                          NodeDetails, LinkDetails>;
+    using node_t = graph_t::vertex_descriptor;
+    using link_t = graph_t::edge_descriptor;
+    
+    explicit BoostGraph (const LemonGraph& lemonGraph);
+
+private:
+    void GenerateBoostGraph(const LemonGraph& lemonGraph);
+    void GenerateBoostNodes(const LemonGraph& lemonGraph);
+    void GenerateBoostLinks(const LemonGraph& lemonGraph);
+    
+    graph_t m_graph;
+    std::map<id_t, node_t> m_nodeMap;
+    std::map<id_t, link_t> m_linkMap;
+};
+
+#endif /* boostgraph_hpp */
