0936e45f6af5387bbc55fd9684d090126ff4d177
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2019-02-15 18:58:11 +0100

    Remove unnecessary spaces

--- KShortestPath/src/main.cpp
+++ KShortestPath/src/main.cpp
@@ -34,15 +34,15 @@ int main(int argc, const char * argv[]) {
                        "one path to simulate OSPF")
                       ("verbose,v", po::bool_switch(&verbose),
                        "Enable verbose output.");
-        
+
         po::variables_map vm;
         po::store(po::parse_command_line(argc, argv, cmdLineParams), vm);
-        
+
         if (vm.count("help")) { // Output help message
             std::cout << cmdLineParams << "\n";
             return 0;
         }
-        
+
         po::notify(vm); // Check if all parameters required were passed
 
         if (!perFlowK && globalK <= 0) {
@@ -75,6 +75,6 @@ int main(int argc, const char * argv[]) {
         std::cerr << "Error occurred" << "\n";
         return -1;
     }
-    
+
     return 0;
 }
