81c51478fff7866f344a372f301abb7c273edddd
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2019-10-31 19:12:35 +0100

    Add the twoDiamond and lineParallel tests

--- PathSelector/tests/test.py
+++ PathSelector/tests/test.py
@@ -170,10 +170,10 @@ class PathSelectorTestClass(unittest.TestCase):
 
     def verifySetup(self, pa: PathAnalyser, k: int):
         """Run all verification tests available"""
-        self.assertTrue(pa.VerifyAckPaths())
-        self.assertTrue(pa.VerifyPathCost())
-        self.assertTrue(pa.VerifyNetworkTopology())
-        self.assertTrue(pa.VerifyNumPaths(k))
+        self.assertTrue(pa.VerifyAckPaths(), "ACK Paths verification failed")
+        self.assertTrue(pa.VerifyPathCost(), "Path Cost verification failed")
+        self.assertTrue(pa.VerifyNetworkTopology(), "Network Topology verification failed")
+        self.assertTrue(pa.VerifyNumPaths(k), "Number of paths verification failed")
 
     def runPathSelector(self, topology: str, algorithm: str, k: int) -> str:
         """Run the PathSelector algorithm with the given parameters"""
@@ -205,18 +205,21 @@ class PathSelectorTestClass(unittest.TestCase):
         return self.verifyResultFile(resultFile, k)
 
     def verifyRandomPathSelection(self, topology: str, algorithm: str, k: int, flowId: int,
-                                  paths: List[List[int]], maxIterations: int = 50) -> bool:
+                                  paths: List[List[int]], verifyResultFile: bool = True,
+                                  maxIterations: int = 50) -> bool:
         """
         Ensure that all the paths in the list are at least found once by the
         algorithm. This function is used to test the random path selection
         mechanism when there are multiple paths available with equal cost to the
-        kth path.
+        kth path. The verifyResultFile option is given because in the case of parallel links the ACK
+        verification may fail.
 
         :param topology: The topology to run
         :param algorithm: The algorithm to use
         :param k: The number of paths to find
         :param flowId: The flow to test
         :param paths: The list of paths to compare to
+        :param verifyResultFile: When set to true verify the generated result file.
         :param maxIterations: The maximum number of iterations to try before
                               failing the test
 
@@ -226,7 +229,11 @@ class PathSelectorTestClass(unittest.TestCase):
 
         for _ in range(0, maxIterations):
             resultFile = self.runPathSelector(topology, algorithm, k)
-            pa = self.verifyResultFile(resultFile, k)
+            pa = None
+            if verifyResultFile:
+                pa = self.verifyResultFile(resultFile, k)
+            else:
+                pa = PathAnalyser(resultFile)
 
             for pathIndex, pathLinks in enumerate(paths):
                 if pa.DataPathExists(flowId, pathLinks) is True:
@@ -237,7 +244,7 @@ class PathSelectorTestClass(unittest.TestCase):
 
         return False
 
-    @timeout_decorator.timeout(5, use_signals=False)
+    @timeout_decorator.timeout(1, use_signals=False)
     def testDiamond(self):
         """Test the Diamond topology for various k values"""
         for k in [1, 2, 5]:
@@ -336,6 +343,64 @@ class PathSelectorTestClass(unittest.TestCase):
             self.assertTrue(pa.AckPathExists(0, [1, 9, 21, 27]))
             self.assertTrue(pa.AckPathExists(0, [1, 11, 23, 27]))
             self.assertTrue(pa.AckPathExists(0, [1, 13, 25, 27]))
+
+    @timeout_decorator.timeout(1, use_signals=False)
+    def testTwoDiamond(self):
+        k = 20
+
+        algorithm = "KSP"
+        pa = self.runAndVerify("twoDiamond", algorithm, k)
+        self.assertTrue(pa.DataPathExists(0, [0, 2, 6, 10, 12, 16, 20]))
+        self.assertTrue(pa.DataPathExists(0, [0, 4, 8, 10, 14, 18, 20]))
+        self.assertTrue(pa.DataPathExists(0, [0, 2, 6, 10, 14, 18, 20]))
+        self.assertTrue(pa.DataPathExists(0, [0, 4, 8, 10, 12, 16, 20]))
+
+        self.assertTrue(pa.AckPathExists(0, [1, 3, 7, 11, 13, 17, 21]))
+        self.assertTrue(pa.AckPathExists(0, [1, 5, 9, 11, 13, 17, 21]))
+        self.assertTrue(pa.AckPathExists(0, [1, 3, 7, 11, 15, 19, 21]))
+        self.assertTrue(pa.AckPathExists(0, [1, 5, 9, 11, 15, 19, 21]))
+
+        algorithm = "RED"
+        pa = self.runAndVerify("twoDiamond", algorithm, k)
+        self.assertTrue(pa.DataPathExists(0, [0, 4, 8, 10, 14, 18, 20]))
+        self.assertTrue(pa.AckPathExists(0, [1, 5, 9, 11, 15, 19, 21]))
+
+        algorithm = "ED"
+        pa = self.runAndVerify("twoDiamond", algorithm, k)
+        self.assertTrue(pa.DataPathExists(0, [0, 4, 8, 10, 14, 18, 20]))
+        self.assertTrue(pa.AckPathExists(0, [1, 5, 9, 11, 15, 19, 21]))
+
+    @timeout_decorator.timeout(1, use_signals=False)
+    def testLineParallel(self):
+        """Test the lineParallel topology for various k values
+
+        ACK verification is missing due to the problem caused in determining
+        which ACK path to choose when having parallel links. This is not a very
+        important feature; and thus is ruled as an exception in this case and
+        treated as such.
+        """
+        for k in [1, 2, 5]:
+            for algorithm in ["KSP", "RED", "ED"]:
+                if k == 1:
+                    self.assertTrue(self.verifyRandomPathSelection("lineParallel", algorithm, k, 0,
+                                                                   [[0, 2, 6], [0, 4, 6]],
+                                                                   verifyResultFile=False))
+                    resultFile = self.runPathSelector("lineParallel", algorithm, k)
+                    pa = PathAnalyser(resultFile)
+                    self.assertTrue(pa.VerifyPathCost(), "Path Cost verification failed")
+                    self.assertTrue(pa.VerifyNetworkTopology(),
+                                    "Network Topology verification failed")
+                    self.assertTrue(pa.VerifyNumPaths(k), "Number of paths verification failed")
+                else:
+                    resultFile = self.runPathSelector("lineParallel", algorithm, k)
+                    pa = PathAnalyser(resultFile)
+                    self.assertTrue(pa.VerifyPathCost(), "Path Cost verification failed")
+                    self.assertTrue(pa.VerifyNetworkTopology(),
+                                    "Network Topology verification failed")
+                    self.assertTrue(pa.VerifyNumPaths(k), "Number of paths verification failed")
+
+                    self.assertTrue(pa.DataPathExists(0, [0, 2, 6]))
+                    self.assertTrue(pa.DataPathExists(0, [0, 4, 6]))
 
 
 if __name__ == "__main__":
