084ac77a9f57ddcc34f79ea5c3df3ec60ce30df6
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2019-11-03 10:15:50 +0100

    Update build.sh to separate building from configuration

--- PathSelector/build.sh
+++ PathSelector/build.sh
@@ -2,27 +2,44 @@
 
 script_path="$( cd "$(dirname "$0")" ; pwd -P )"
 
-function build_debug {
+function configureDebug {
   cd ${script_path} && mkdir -p build/debug
-  cd ${script_path} && cd build/debug && cmake -DCMAKE_BUILD_TYPE=Debug ../../ && make
+  cd ${script_path} && cd build/debug && cmake -G'Unix Makefiles' -DCMAKE_BUILD_TYPE=Debug  ../../
 }
 
-function build_release {
-  cd ${script_path} && mkdir -p build/release
-  cd ${script_path} && cd build/release && cmake -DCMAKE_BUILD_TYPE=Release ../../ && make
+function configureRelease {
+  cd ${script_path} && mkdir -p build/debug
+  cd ${script_path} && cd build/debug && cmake -G'Unix Makefiles' -DCMAKE_BUILD_TYPE=Release  ../../
+}
+
+function buildDebug {
+  cd ${script_path}/build/debug && make
+}
+
+function buildRelease {
+  cd ${script_path}/build/release && make
 }
 
 if [ "$1" == debug ] ; then
-  build_debug
+  configureDebug
+  buildDebug
+elif [ "$1" == configureDebug ] ; then
+  configureDebug
+elif [ "$1" == buildDebug ] ; then
+  buildDebug
 elif [ "$1" == release ] ; then
-  build_release
+  configureRelease
+  buildRelease
+elif [ "$1" == configureRelease ] ; then
+  configureRelease
+elif [ "$1" == buildRelease ] ; then
+  buildDebug
 elif [ "$1" == clean ] ; then
   rm -rf build
-  mkdir -p build/debug
-  mkdir -p build/release
-elif [ "$1" == cleanall ] ; then
-  rm -rf build
 else # Default build both debug and release
-  build_debug
-  build_release
+  configureDebug
+  configureRelease
+
+  buildDebug
+  buildRelease
 fi
